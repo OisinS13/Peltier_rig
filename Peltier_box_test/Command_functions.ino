0f13b3a420ada8c8168192fe41f44306bfc2f736
Holland, Thomas J <thomas.holland17@imperial.ac.uk>
2023-09-29 12:20:59 +0100

    Uncomment save_settings

--- Peltier_box_test/Command_functions.ino
+++ Peltier_box_test/Command_functions.ino
@@ -6,18 +6,18 @@ void Cmd_Reinitialise(CommandParameter& Parameters) {
   rp2040.restartCore1();
 }
 
-// void Cmd_Save_settings(CommandParameter &Parameters) {
-//  const char *Filename_from_CMD=Parameters.NextParameter();
-//     // if (Verbose_output) {
-//     //   Serial.print("Filename: ");
-//     //   Serial.println(Filename_from_CMD);
-//     // }
-//   if (Save_settings_to_file(Filename_from_CMD)) {
-//     if (Verbose_output) {
-//       Serial.println("Settings saved");
-//     }
-//   }
-// }
+void Cmd_Save_settings(CommandParameter &Parameters) {
+ const char *Filename_from_CMD=Parameters.NextParameter();
+    // if (Verbose_output) {
+    //   Serial.print("Filename: ");
+    //   Serial.println(Filename_from_CMD);
+    // }
+  if (Save_settings_to_file(Filename_from_CMD)) {
+    if (Verbose_output) {
+      Serial.println("Settings saved");
+    }
+  }
+}
 
 bool isValidLocation(const char* input, int& board, int& channel) {
   if (strcmp(input, "all") == 0) {
